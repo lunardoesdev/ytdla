0b868da3f3845d911dec47fa80164afae0b4d565
Lunar <lunarlifeburst@gmail.com>
2026-03-26 16:52:12 +1000

    bash to python

--- src/ytdla/__init__.py
+++ src/ytdla/__init__.py
@@ -1,2 +1,23 @@
+import sys
+import os
+
 def main():
-    print("hi")
+    links = sys.argv[1:]
+    for link in links:
+        cmd = "yt-dlp"
+        cmd += " --no-playlist"
+        cmd += " --extract-audio"
+        cmd += " --audio-format mp3"
+        cmd += " --embed-thumbnail"
+        cmd += " --embed-metadata"
+        cmd += " --embed-chapters"
+        cmd += " --embed-info-json"
+        cmd += " --format '251/140/bestaudio/mp3/91/best'"
+        cmd += " --sponsorblock-remove all"
+        cmd += " --age-limit 99"
+        cmd += " --write-info-json"
+        cmd += " --concurrent-fragments 12"
+        cmd += " -o '%(channel,series,uploader,playlist_title,title)s - %(title)s.%(ext)s'"
+        cmd += " '" + link + "'"
+
+        os.system(cmd)
