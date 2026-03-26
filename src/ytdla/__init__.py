db8312c6f90d95b42975de7d277cd5ce463b04a7
Lunar <lunarlifeburst@gmail.com>
2026-03-26 17:12:45 +1000

    print message when no links are passed

--- src/ytdla/__init__.py
+++ src/ytdla/__init__.py
@@ -3,6 +3,8 @@ import os
 
 def main():
     links = sys.argv[1:]
+    if len(links == 0):
+        print('pass links to the script, like: yt-dlp "https://www.youtube.com/watch?v=TeSgUmh_7jc"')
     for link in links:
         cmd = "yt-dlp"
         cmd += " --no-playlist"
